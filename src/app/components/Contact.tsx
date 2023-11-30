ad30dd85102d6c4695c52ad3fce51d4db9937b95
IngridGdesigns <garciai@protonmail.com>
2023-11-30 15:40:11 -0800

    undo html button

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -3,12 +3,14 @@ import * as React from 'react'
 
 export default function Contact() {
     return (
-        <div>
-            <input type="text" placeholder="first name" />
-            <input type="text" placeholder="last name" />
-            <input type="text" placeholder="reachMe@example.com" />
-            <textarea name="content" rows={4} cols={40} />
-            <input type="submit" value="Submit"/>
-        </div>
+        <form>
+            <div>
+                <input type="text" placeholder="first name" />
+                <input type="text" placeholder="last name" />
+                <input type="text" placeholder="reachMe@example.com" />
+                <textarea name="content" rows={4} cols={40} />
+                <input type="submit" value="Submit"/>
+            </div>
+        </form>
     )
 }
