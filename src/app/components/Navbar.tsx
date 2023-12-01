4e30da7f5a2c1626612cbf955e33609be91667d6
IngridGdesigns <garciai@protonmail.com>
2023-12-01 04:58:14 -0800

    added navbar and css

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
+import styles from '../page.module.css'
 
 export default function Navbar() {
     return (
         <nav>
-            <h2>Nav stuff</h2>
+            <h2>Nav logo stuff</h2>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
         </nav>
     )
 }
