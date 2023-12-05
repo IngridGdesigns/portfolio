df7759afa7bf35707e46e99772dc8676898be688
IngridGdesigns <garciai@protonmail.com>
2023-12-05 02:18:53 -0800

    html section

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -3,14 +3,16 @@ import * as React from 'react'
 
 export default function Contact() {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="first name" />
-                <input type="text" placeholder="last name" />
-                <input type="text" placeholder="reachMe@example.com" />
-                <textarea name="content" rows={4} cols={40} />
-                <input type="submit" value="Submit"/>
-            </div>
-        </form>
+        <section >
+            <form>
+                <div>
+                    <input type="text" placeholder="first name" />
+                    <input type="text" placeholder="last name" />
+                    <input type="text" placeholder="reachMe@example.com" />
+                    <textarea name="content" rows={4} cols={40} />
+                    <input type="submit" value="Submit"/>
+                </div>
+            </form>
+         </section>
     )
 }
