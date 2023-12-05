940eebfa390ff6e7aa9cc346f106d8c8b9b054ee
IngridGdesigns <garciai@protonmail.com>
2023-12-05 02:15:02 -0800

    added lipsum lorem placeholder text

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,10 +1,43 @@
 import * as React from 'react'
+import styles from '../page.module.css'
+import Image from 'next/image'
 
 export default function Projects() {
     return (
-        <section>
+        <div className={styles.greeting}>
             <h1>Hello from my Projects Section</h1>
-            <p>my awesome projects</p>
-        </section>
+            <div className={styles.responsiveTwoColumns}>
+                <div>
+                    <Image src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div>
+                    <Image src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                 <div>
+                    <Image src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div>
+                    <Image src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,6 +4,10 @@ export default function Skills() {
     return (
         <section>
             <h1>My Skills</h1>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+            praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
+            excepturi sint occaecati cupiditate non provident,similique sunt in culpa
+            qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
             <ul>
                 <li></li>
             </ul>
