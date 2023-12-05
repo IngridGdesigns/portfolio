e4cd970429b6ca90a619b6ad2929405384acfd33
IngridGdesigns <garciai@protonmail.com>
2023-12-05 02:18:30 -0800

    added styles

--- src/app/components/Greetings.tsx
+++ src/app/components/Greetings.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import styles from '../page.module.css'
 
 export default function Greetings() {
     return (
-        <section>
+        
+        <section className={styles.greeting}>
             <h1>Hello and Welcome Section</h1>
             <p>testing testing, 1, 2, 3....</p>
-        </section>
+         </section>
     )
 }
