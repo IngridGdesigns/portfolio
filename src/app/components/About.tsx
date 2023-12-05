9b7fcd21aa6047d273bc9dd9e3f9eff92a9d5c2a
IngridGdesigns <garciai@protonmail.com>
2023-12-05 02:18:17 -0800

    added placeholder to component

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react'
+import styles from '../page.module.css'
+import Image from 'next/image'
 
 export default function About() {
     return (
-        <section>
+        <section className={styles.row}>
+            <div className={styles.column}>
             <h1>About Me Section</h1>
-            <p>This is me, this is what I do, this is what I been up too.</p>
+                <p>This is me, this is what I do, this is what I been up too.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div className={styles.column}>
+                <Image src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
             <ul>
                 <li>GitHub link</li>
                 <li>LinkedIn link</li>
 
-            </ul>
+                </ul>
+            </div>
         </section>
     )
 }
