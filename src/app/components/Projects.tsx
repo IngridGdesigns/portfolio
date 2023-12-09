ebd7f49a170f0729e92836bf0fcc1061721df8ef
IngridGdesigns <garciai@protonmail.com>
2023-12-09 04:14:32 -0800

    passing props

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -4,25 +4,26 @@ import Image from 'next/image'
 import { ProjectProps } from "../page";
 
 export default function Projects({ projects }: ProjectProps) {
-    const project = projects[0];
+
+    const mappProjects = projects.map(project => {
+        return (
+            <div key={project.id}>
+                <Image className={styles.responsiveImage} src={`/images/${project.coverImg}`} alt={project.alt} width={500} height={400} />
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+            </div>
     
+        )
+    })
+
     return (
         <div className={styles.greeting}>
             
             <h1>Hello from my Projects Section</h1>
+
             <div className={styles.responsiveTwoColumns}>
-                <div>
-                    <Image className={styles.responsiveImage} src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
-                    hello {project.title}
-                </div>
-                <div>
-                    <Image className={styles.responsiveImage} src={`/images/catWallpaper.jpg`} alt="cat wearing sunglasses" width={500} height={400} />
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                </div>
+                {mappProjects}
+           
             </div>
         </div>
     )
